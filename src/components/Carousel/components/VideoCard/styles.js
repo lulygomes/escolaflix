7dd957eb4fcff377b11af014d5458f6db5c338bc
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-07-28 22:44:59 -0300

    Atualização do Carousel

--- src/components/Carousel/components/VideoCard/styles.js
+++ src/components/Carousel/components/VideoCard/styles.js
@@ -22,7 +22,12 @@ export const VideoCardContainer = styled.a`
   transition: opacity .3s;
   &:hover,
   &:focus {
-    opacity: .5;
+    /* opacity: .5; */
+    -webkit-transform: scale(1.05);
+    -moz-transform: scale(1.05);
+    -o-transform: scale(1.05);
+    -ms-transform: scale(1.05);
+    transform: scale(1.05);
   }
   
   &:not(:first-child) {
