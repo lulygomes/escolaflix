7784285ecee58859504f849680eb9358def00bed
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-08-03 11:33:29 -0300

    Aula 4

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
-import Button from '../Button'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+// eslint-disable-next-line import/no-named-as-default
+import Button from '../Button';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <Link to="/">
-                <img className='Logo' src={Logo} alt="EscolaFlix logo"/>
-            </Link>
-            
-            <Button as={ Link } to="/cadastro/video" className="ButtonLink">
-                Novo vídeo
-            </Button>
-        </nav>
-    )
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="EscolaFlix logo" />
+      </Link>
+
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
--- src/components/pages/cadastro/Categoria/index.jsx
+++ src/components/pages/cadastro/Categoria/index.jsx
@@ -1,96 +1,94 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import PageDefault from '../../../PageDefault'
-import FormField from '../../../FormField'
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../PageDefault';
+import FormField from '../../../FormField';
+import Button from '../../../Button';
 // import Form from './styles'
 
 function CadastroCategoria() {
-    const valorInicial = {
-        nome: '',
-        descricao:'',
-        cor:'#000',
-    }
+  const valorInicial = {
+    nome: '',
+    descricao: '',
+    cor: '#000',
+  };
 
-    const [values, setValues ] = useState(valorInicial)
-    const [categorias, setCategorias] = useState([])
+  const [values, setValues] = useState(valorInicial);
+  const [categorias, setCategorias] = useState([]);
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        setCategorias([
-            ...categorias,
-            values
-        ]);
-        setValues(valorInicial)
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    setCategorias([...categorias, values]);
+    setValues(valorInicial);
+  }
 
-    function setValue(chave, valor) {
-        setValues({
-            ...values,
-            [chave]: valor,
-        })
-    }
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
 
-    function handleChange(e) {
-        const { name, value } = e.target
-        setValue( name, value )
-    }
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setValue(name, value);
+  }
 
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-            <hr/>
-            
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <FormField 
-                    label="Nome"
-                    type="text"
-                    name="nome"
-                    value={values.nome}
-                    placeholder="Nome da Categoria"
-                    onChange={handleChange}
-                />
-                
-                <FormField 
-                    label="Descrição"
-                    type="text"
-                    name="descricao"
-                    value={values.descricao}
-                    placeholder="Descrição da Categoria"
-                    onChange={handleChange}
-                />
-                
-                <FormField
-                    label="Cor"
-                    type="color"
-                    name="cor"
-                    value={values.cor}
-                    onChange={handleChange}
-                />
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+      <hr />
 
-                <button>
-                    Cadastrar
-                </button>
-            </form>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <FormField
+          label="Nome"
+          type="text"
+          name="nome"
+          value={values.nome}
+          placeholder="Nome da Categoria"
+          onChange={handleChange}
+        />
 
-            <div>
-                <ul>
-                    {categorias.map((categoria, indice) => {
-                        return (
-                            <li key={indice} >
-                                {categoria.nome} | {categoria.descricao} | {categoria.cor}
-                            </li>
-                        )
-                    }
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          placeholder="Descrição da Categoria"
+          onChange={handleChange}
+        />
 
-                    )}
-                </ul>
-            </div>
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
 
-            <Link to='/'>
-                Ir para home
-            </Link>
-        </PageDefault>
-    )
+        <Button>Cadastrar</Button>
+      </form>
+
+      <div>
+        <ul>
+          {categorias.map((categoria, indice) => (
+            <li key={`${indice}${categoria}`}>
+              {categoria.nome}
+              {' '}
+              |
+              {categoria.descricao}
+              {' '}
+              |
+              {categoria.cor}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Link to="/">Ir para home</Link>
+    </PageDefault>
+  );
 }
 
-export default CadastroCategoria
+export default CadastroCategoria;
